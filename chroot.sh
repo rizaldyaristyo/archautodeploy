f5fb9f4ed508f4ba8330552af5ae54cb85aff0e0
kblk <84843856+kblkLab@users.noreply.github.com>
2021-09-13 00:54:25 +0700

    Changed to Append
    File input changed to append instead of overwrite
    reboot warning also added to avoid confussion

--- chroot.sh
+++ chroot.sh
@@ -3,9 +3,9 @@ echo arch-chroot environment entered!
 ln -sf /usr/share/zoneinfo/UTC
 hwclock --systohc
 pacman -Sy nano --noconfirm
-echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo $archUName > /etc/hostname
 echo -e "127.0.0.1      localhost\n::1            localhost\n127.0.1.1      my.localdomain      $archUName">/etc/hosts
 pacman -Sy netctl dialog dhcpcd wpa_supplicant ifplugd --noconfirm
@@ -15,6 +15,6 @@ pacman -Sy grub efibootmgr os-prober --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=arch_grub
 grub-mkconfig -o /boot/grub/grub.cfg
 echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub
-echo Please enter a password for root user
+echo Please enter a password for the root user before reboot
 passwd
 exit
