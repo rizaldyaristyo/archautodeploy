a1c20995241057c63a805431aa04d20f6107ff3b
rizaldyaristyo <anandarizaldyaristyo@gmail.com>
2024-10-16 08:32:58 +0700

    checked DNS connectivity, added netctl autoconfigure, is now able to boot via bios

---
+++ aad.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Copyleft 2021 Aristyo
+source aad.conf
+rootSize = ${ROOT_PARTITION_SIZE}
+
+clear
+echo '
+     ____ ____ ____ 
+    ||A |||A |||D ||
+    ||__|||__|||__||
+    |/__\|/__\|/__\|.sh
+'
+if [ -f aad.conf ]; then
+    echo OK! - aad.conf present!
+else
+    echo BAD - aad.conf is missing!
+    echo MSG - Please re-clone the repo and try again
+    exit 1
+fi
+if [ "$CHECKS" = true ]; then
+    echo Checking...
+    if [ -d /sys/firmware/efi ]; then
+		echo OK! - UEFI Confirmed!
+    else
+		echo BAD - UEFI Only! Legacy BIOS not supported.
+		echo MSG - If this you think that this was a mistake please modify the script variables manually
+		exit 1 
+    fi
+    if [ -f chroot.sh ]; then
+		cho OK! - chroot script present!
+    else
+		echo BAD - chroot script is missing!
+		echo MSG - Please re-clone the repo and try again as the file needed in chroot process is missing
+		exit 1
+    fi
+    if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+		echo OK! - IPv4 is up
+		if ping -q -c 1 -W 1 google.com >/dev/null; then
+			echo OK! - DNS is up
+		else
+			echo BAD - DNS is down
+			echo MSG - Please configure your DNS, if you think that this was a mistake, please modify the script variables manually
+			exit 1
+		fi
+    else
+		echo BAD - IPv4 is down
+		echo MSG - Please connect to the internet via iwctl, if you think that this was a mistake, please modify the script variables manually
+		exit 1
+    fi
+fi
+echo;echo;
+echo ______________________________________WARNING!______________________________________
+echo THIS WILL WIPE YOUR SELECTED DRIVE AND INSTALL ARCH LINUX AS THE ONLY OS
+echo THIS SCRIPT DISTRIBUTED AS IS AND PLEASE PROCEED AT YOUR OWN RISK.
+echo ____________________________________________________________________________________
+echo;echo;
+echo Your arch username will be $ARCH_USERNAME
+echo;echo;
+sfdisk -l ${DRIVE_TO_USE_AND_WIPE}
+echo;echo;
+echo "You entered ${DRIVE_TO_USE_AND_WIPE}. This will delete all the data in the drive ${DRIVE_TO_USE_AND_WIPE}"
+echo "This'll create ${DRIVE_TO_USE_AND_WIPE}1, ${DRIVE_TO_USE_AND_WIPE}2, ${DRIVE_TO_USE_AND_WIPE}3, and ${DRIVE_TO_USE_AND_WIPE}4 afterward"
+echo;
+read -p "Are you sure you want to continue? (THERE'S NO TURNING BACK!) (y/N): " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]];then
+    echo;echo;
+    echo Working...
+    echo;echo;
+    wipefs -a ${DRIVE_TO_USE_AND_WIPE} # drive wipe
+    (echo o;echo y;echo w;echo y;) | gdisk ${DRIVE_TO_USE_AND_WIPE} # new GPT table
+    
+    # Partitioning, tested on fdisk (util-linux 2.40.2)
+    (echo n;echo;echo;echo +550M;echo y;echo t;echo ;echo 1;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT EFI partition
+    (echo n;echo;echo;echo +350M;echo y;echo t;echo ;echo 4;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT BIOS partition
+    (echo n;echo;echo;echo +${rootSize};echo y;echo t;echo;echo 20;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # root partition
+    (echo n;echo;echo;echo +2G;echo y;echo t;echo;echo 19;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # swap partition
+
+    # Formatting
+    mkfs.vfat -F 32 ${DRIVE_TO_USE_AND_WIPE}1 # Format EFI partition (Tested on mkfs.fat 4.2)
+    (echo "set 2 bios_grub on") | parted ${DRIVE_TO_USE_AND_WIPE} # Format BIOS partition (Tested on GNU Parted 3.6)
+    mkfs.ext4 ${DRIVE_TO_USE_AND_WIPE}3 # Format root partition (Tested on mke2fs 1.47.1)
+    mkswap ${DRIVE_TO_USE_AND_WIPE}4 # Format swap partition
+    swapon ${DRIVE_TO_USE_AND_WIPE}4 # Enable swap partition
+
+    # Mounting
+    mount /dev/sda3 /mnt # Mount root partition
+    mkdir /mnt/efi # Create EFI mount point
+    mount /dev/sda1 /mnt/efi # Mount EFI partition
+
+    pacstrap /mnt base linux linux-firmware # Install base system
+    genfstab -U /mnt >> /mnt/etc/fstab # Generate fstab
+    cp chroot.sh /mnt # Copy chroot script to /mnt
+    chmod +xrw /mnt/chroot.sh # Make chroot script executable
+    echo Continuing in chroot...
+    export ARCH_USERNAME
+    export ARCH_PASSWORD
+    export DRIVE_TO_USE_AND_WIPE
+    export ROOT_PASSWORD
+    arch-chroot /mnt ./chroot.sh # Run chroot script
+    clear
+    echo chroot setup complete!
+    echo Rebooting...
+    reboot
+fi
+echo;echo Exitting...
+exit 1
--- chroot.sh
+++ chroot.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 echo arch-chroot environment entered!
-ln -sf /usr/share/zoneinfo/UTC # Configure the time zone
-hwclock --systohc # Set the hardware clock
-pacman -Sy nano --noconfirm # Install nano
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen # Generate locale
-locale-gen # Generate locale
-echo LANG=en_US.UTF-8 >> /etc/locale.conf # Set the locale
-echo $archUName > /etc/hostname # Set the hostname
-echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 my.localdomain $archUName">/etc/hosts # Add matching entries to hosts file
-pacman -Sy netctl dialog dhcpcd wpa_supplicant ifplugd --noconfirm # Install network manager
-useradd -G wheel -m $archUName # Create user
-echo "$archUName:$archPasswd" | chpasswd # Set password for user
+ln -sf /usr/share/zoneinfo/UTC # configure time zone
+hwclock --systohc # set hardware clock
+pacman -Sy nano neofetch --noconfirm # BECAUSE I LOVE NANO and you do want to show off right?
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen # gen locale
+locale-gen # gen locale
+echo LANG=en_US.UTF-8 >> /etc/locale.conf # set locale
+
+# User and Host Setup
+echo ${ARCH_USERNAME} > /etc/hostname # Set the hostname
+echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 my.localdomain ${ARCH_USERNAME}">/etc/hosts # add loopback entries to hosts file
+pacman -Sy netctl dialog dhcpcd wpa_supplicant ifplugd --noconfirm # install network managers
+useradd -G wheel -m ${ARCH_USERNAME} # create user
+echo "${ARCH_USERNAME}:${ARCH_PASSWORD}" | chpasswd # set password
+
+# Install Grub
 pacman -Sy grub efibootmgr os-prober --noconfirm # Install grub
-grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=arch_grub # Install grub
+grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=arch_grub # install EFI grub
+grub-install --target=i386-pc ${DRIVE_TO_USE_AND_WIPE} # install BIOS grub
 grub-mkconfig -o /boot/grub/grub.cfg # Generate grub config
 echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub # Enable grub os-prober
-echo Please enter a password for the root user before reboot
-passwd # Set password for root
+
+# Configure network to use after reboot
+networkIfaceInUse=$(ip -o -4 route show to default | awk '{print $5}')
+if [ -d "/sys/class/net/${networkIfaceInUse}" ]; then
+    echo "No active network interface found, Skipping..."
+else
+    echo "creating netctl configuration for interface ${networkIfaceInUse}..."
+    echo -e "Description='A simple DHCP connection for ${networkIfaceInUse}'\nInterface=${networkIfaceInUse}\nConnection=ethernet\nIP=dhcp" > "/etc/netctl/${networkIfaceInUse}-dhcp"
+    netctl enable ${networkIfaceInUse}-dhcp
+fi
+
+# Set root password
+echo ; echo ;
+echo "ALL DONE! - Setting root password..."
+echo "${ROOT_PASSWORD}" | passwd --stdin root
+echo "Exitting arch-chroot..."
 exit
