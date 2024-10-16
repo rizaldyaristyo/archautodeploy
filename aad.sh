81c5b638d87d25241b658cbcbb0b0896ab1f76b0
rizaldyaristyo <anandarizaldyaristyo@gmail.com>
2024-10-16 08:53:35 +0700

    fixed typos

--- aad.sh
+++ aad.sh
@@ -28,14 +28,14 @@ if [ "$CHECKS" = true ]; then
 		exit 1 
     fi
     if [ -f chroot.sh ]; then
-		cho OK! - chroot script present!
+		  echo OK! - chroot script present!
     else
-		echo BAD - chroot script is missing!
-		echo MSG - Please re-clone the repo and try again as the file needed in chroot process is missing
-		exit 1
+      echo BAD - chroot script is missing!
+      echo MSG - Please re-clone the repo and try again as the file needed in chroot process is missing
+		  exit 1
     fi
     if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-		echo OK! - IPv4 is up
+		  echo OK! - IPv4 is up
 		if ping -q -c 1 -W 1 google.com >/dev/null; then
 			echo OK! - DNS is up
 		else
@@ -44,37 +44,37 @@ if [ "$CHECKS" = true ]; then
 			exit 1
 		fi
     else
-		echo BAD - IPv4 is down
-		echo MSG - Please connect to the internet via iwctl, if you think that this was a mistake, please modify the script variables manually
-		exit 1
+      echo BAD - IPv4 is down
+      echo MSG - Please connect to the internet via iwctl, if you think that this was a mistake, please modify the script variables manually
+      exit 1
     fi
 fi
-echo;echo;
+echo; echo;
 echo ______________________________________WARNING!______________________________________
 echo THIS WILL WIPE YOUR SELECTED DRIVE AND INSTALL ARCH LINUX AS THE ONLY OS
 echo THIS SCRIPT DISTRIBUTED AS IS AND PLEASE PROCEED AT YOUR OWN RISK.
 echo ____________________________________________________________________________________
-echo;echo;
+echo; echo;
 echo Your arch username will be $ARCH_USERNAME
-echo;echo;
+echo; echo;
 sfdisk -l ${DRIVE_TO_USE_AND_WIPE}
-echo;echo;
+echo; echo;
 echo "You entered ${DRIVE_TO_USE_AND_WIPE}. This will delete all the data in the drive ${DRIVE_TO_USE_AND_WIPE}"
 echo "This'll create ${DRIVE_TO_USE_AND_WIPE}1, ${DRIVE_TO_USE_AND_WIPE}2, ${DRIVE_TO_USE_AND_WIPE}3, and ${DRIVE_TO_USE_AND_WIPE}4 afterward"
 echo;
 read -p "Are you sure you want to continue? (THERE'S NO TURNING BACK!) (y/N): " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]];then
-    echo;echo;
+    echo; echo;
     echo Working...
-    echo;echo;
+    echo; echo;
     wipefs -a ${DRIVE_TO_USE_AND_WIPE} # drive wipe
-    (echo o;echo y;echo w;echo y;) | gdisk ${DRIVE_TO_USE_AND_WIPE} # new GPT table
+    (echo o; echo y; echo w; echo y;) | gdisk ${DRIVE_TO_USE_AND_WIPE} # new GPT table
     
     # Partitioning, tested on fdisk (util-linux 2.40.2)
-    (echo n;echo;echo;echo +550M;echo y;echo t;echo ;echo 1;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT EFI partition
-    (echo n;echo;echo;echo +350M;echo y;echo t;echo ;echo 4;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT BIOS partition
-    (echo n;echo;echo;echo +${rootSize};echo y;echo t;echo;echo 20;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # root partition
-    (echo n;echo;echo;echo +2G;echo y;echo t;echo;echo 19;echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # swap partition
+    (echo n; echo; echo; echo +550M; echo y; echo t; echo ; echo 1; echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT EFI partition
+    (echo n; echo; echo; echo +350M; echo y; echo t; echo ; echo 4; echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # BOOT BIOS partition
+    (echo n; echo; echo; echo +${rootSize}; echo y; echo t; echo; echo 20; echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # root partition
+    (echo n; echo; echo; echo +2G; echo y; echo t; echo; echo 19; echo w) | fdisk ${DRIVE_TO_USE_AND_WIPE} # swap partition
 
     # Formatting
     mkfs.vfat -F 32 ${DRIVE_TO_USE_AND_WIPE}1 # Format EFI partition (Tested on mkfs.fat 4.2)
@@ -103,5 +103,5 @@ if [[ $REPLY =~ ^[Yy]$ ]];then
     echo Rebooting...
     reboot
 fi
-echo;echo Exitting...
+echo; echo Exitting...
 exit 1
